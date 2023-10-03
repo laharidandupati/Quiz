0120f57caf81ccceb5e00fa6b7e5273eadbb680e
laharidandupati <109577057+laharidandupati@users.noreply.github.com>
2023-10-03 09:48:24 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+let questions=[
+    {
+        numb:1,
+        question:"Javascript is an __________ language?",
+        answer:"A. Object-Oriented",
+        options:[
+            "A. Object-Oriented",
+            "B. Object-Based",
+            "C. Procedural ",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb:2,
+        question:"Which of the following keywords is used to define a variable in javascript?",
+        answer:"C. Both A and B",
+        options:[
+            "A. var",
+            "B. let",
+            "C. Both A and B",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb:3,
+        question:"Which of the following methods is used to access HTML elements using Javascript?",
+        answer:"C. Both A and B",
+        options:[
+            "A. getElementbyId()",
+            "B. getElementsByClassName()",
+            "C. Both A and B",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb:4,
+        question:"How can a datatype be declared to be a constant type?",
+        answer:"A. const",
+        options:[
+            "A. const",
+            "B. let",
+            "C. var",
+            "D. constant"
+        ]
+    },
+    {
+        numb:5,
+        question:"What Keyword is used to check whether a given property is valid or not?",
+        answer:"A. in",
+        options:[
+            "A. in",
+            "B. is in",
+            "C. exists",
+            "D. lies"
+        ]
+    },
+    {
+        numb:6,
+        question:"Which CSS property allows you to set multiple list properties at once?",
+        answer:"B. List-Style",
+        options:[
+            "A. List-Style-Type",
+            "B. List-Style",
+            "C. List-Style-Position",
+            "D. List-Style-Image"
+        ]
+    },
+    {
+        numb:7,
+        question:"How can you write into HTML output using Java Script?",
+        answer:"C. document.write()",
+        options:[
+            "A. innerHTML",
+            "B. console.log()",
+            "C. document.write()",
+            "D. window.alert()"
+        ]
+    },
+    {
+        numb:8,
+        question:"Elements between and tags of HTML tables are _____ by default?",
+        answer:"B. left aligned",
+        options:[
+            "A. justified",
+            "B. left aligned",
+            "C. right aligned",
+            "D. center aligned"
+        ]
+    },
+    {
+        numb:9,
+        question:"The basic elements of a form are called:?",
+        answer:"D. controls",
+        options:[
+            "A. objects",
+            "B. table",
+            "C. record",
+            "D. controls"
+        ]
+    },
+    {
+        numb:10,
+        question:"What is the latest version of CSS avaliable?",
+        answer:"B. CSS3",
+        options:[
+            "A. CSS2",
+            "B. CSS3",
+            "C. CSS3.1",
+            "D. CSS4"
+        ]
+    },
+    
+];
